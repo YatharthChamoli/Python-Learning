a364fc26c03d99b362e5279d6c0adf16bb0136ed
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-11-26 23:59:28 +0530

    new learning

---
+++ 02_Datatypes/Chapter_3.py
@@ -0,0 +1,10 @@
+# Integer
+
+black_tea_grams = 14  
+ginger_grams = 3
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of base tea is {total_grams}")
+
+remaining_tea = black_tea_grams - ginger_grams
+print(f"Total grams of base tea is {remaining_tea}")
