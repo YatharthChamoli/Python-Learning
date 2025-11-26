7b10f86ec868b93a09b6aa2f49e2f7417af18786
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-11-27 00:22:28 +0530

    learning

--- 02_Datatypes/Chapter_3.py
+++ 02_Datatypes/Chapter_3.py
@@ -8,3 +8,14 @@ print(f"Total grams of base tea is {total_grams}")
 
 remaining_tea = black_tea_grams - ginger_grams
 print(f"Total grams of base tea is {remaining_tea}")
+
+milk_liters = 7 
+servings = 4
+milk_per_serving = milk_liters / servings
+print(f"Milk per serving is {milk_per_serving}")
+
+
+total_tea_bags = 7
+pots = 4
+bags_per_pot = total_tea_bags // pots
+print(f"While tea bags per pot: {bags_per_pot}")
