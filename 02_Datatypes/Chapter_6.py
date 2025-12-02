03f7374400b9fe406e292e06810ad3919e4b88e1
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-12-03 01:30:18 +0530

    learning

---
+++ 02_Datatypes/Chapter_6.py
@@ -0,0 +1,16 @@
+chai_type = "Ginger Chai"
+customer_name = "Priya"
+
+print(f"Order for {customer_name}: {chai_type} please!")
+
+chai_description = "Aromatic and Bold"
+print(f"First word: {chai_description[0:8]}")
+print(f"Last word: {chai_description[12:]}")
+print(f"First word: {chai_description[::-1]}")
+
+label_text = "Chai Spécial"
+encoded_label = label_text.encode("utf-8")
+print(f"Encoded label: {encoded_label}")
+print(f"Non-Encoded label: {label_text}")
+decoded_label = encoded_label.decode("utf-8")
+print(f"Decoded label: {decoded_label}")
