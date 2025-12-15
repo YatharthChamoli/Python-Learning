4a169a6a91fe79e4d5f253fcb0e484b318ca99e7
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-12-16 00:53:53 +0530

    learning

---
+++ 02_Datatypes/Chapter_8.py
@@ -0,0 +1,13 @@
+ingredients = ["water", "milk", "black tea"]
+ingredients.append("sugar")
+print(f"Ingredients are: {ingredients}")
+ingredients.remove("water")
+print(f"Ingredients are: {ingredients}")
+
+spice_options = ["ginger", "cardamom"]
+chai_ingredients = ["water", "milk"]
+
+chai_ingredients.extend(spice_options)
+print(f"chai: {chai_ingredients}")
+chai_ingredients.insert(2, "black tea")
+print(f"chai: {chai_ingredients}")
