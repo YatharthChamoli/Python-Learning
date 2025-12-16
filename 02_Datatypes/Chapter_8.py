db52861ef92959c6ceab168026302ba4c48cc6bc
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-12-17 01:22:36 +0530

    learning

--- 02_Datatypes/Chapter_8.py
+++ 02_Datatypes/Chapter_8.py
@@ -11,3 +11,17 @@ chai_ingredients.extend(spice_options)
 print(f"chai: {chai_ingredients}")
 chai_ingredients.insert(2, "black tea")
 print(f"chai: {chai_ingredients}")
+
+
+base_liquid = ["water", "milk"]
+extra_flavor = ["ginger"]
+
+full_liquid_mix = base_liquid + extra_flavor
+print(f"Liquid mix: {full_liquid_mix}")
+
+strong_brew = ["black tea", "water"] * 3
+print(f"String brew: {strong_brew}")
+
+raw_spice_data = bytearray(b"CINNAMON")
+raw_spice_data = raw_spice_data.replace(b"CINNA", b"CARD")
+print(f"Bytes: {raw_spice_data}")
