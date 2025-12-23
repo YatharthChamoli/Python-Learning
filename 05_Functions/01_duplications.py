cf19838a2a520f1930273f4c77be3238492a9317
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-12-23 23:21:45 +0530

    new chapeter of learning

---
+++ 05_Functions/01_duplications.py
@@ -0,0 +1,16 @@
+# Reducing Code Duplication
+# You're managing a busy tea stall.
+# You receive many orders and want to print each customer's name
+# along with the type of chai they ordered.
+# Task:
+# • Write a function print_order(name, chai_type)
+# • Call it multiple times for different customers
+
+
+def print_order(name, chai_type):
+    print(f"{name} ordered {chai_type} chai.")
+
+print_order("Alice", "Masala")
+print_order("Bob", "Ginger")
+print_order("Charlie", "Cardamom")    
+
