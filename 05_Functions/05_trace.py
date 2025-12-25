b2e06737768ff30d41bc602c94fdb5cd5322e96f
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2025-12-25 22:31:16 +0530

    learning

---
+++ 05_Functions/05_trace.py
@@ -0,0 +1,17 @@
+# Improving Traceability
+# Your shop adds a 10% VAT on every order.
+# You want this to be consistent and traceable.
+# Task:
+# • Write add_vat(price, vat_rate)
+# • Use it to compute final prices for 3 orders
+
+def add_vat(price, vat_rate):
+    return price * (100 + vat_rate) / 100
+
+orders = [50, 100, 200]
+
+for price in orders:
+    final_amount = add_vat(price, 10)
+    print(f"Original price: ${price}, Final amount after VAT: ${final_amount}")
+
+    
