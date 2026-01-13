d35611c23d2fab240c37e7a271e3eeccf286916e
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2026-01-13 22:29:47 +0530

    learning

---
+++ 05_Functions/10_return.py
@@ -0,0 +1,12 @@
+# def make_chai():
+#     # return "Here is your masala chai!"
+#     print("Here is your masala chai!")
+
+# return_value = make_chai()
+
+# print(return_value)
+
+def idel_chaiwala():
+    pass
+
+print(idel_chaiwala())
