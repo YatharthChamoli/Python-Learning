061e72b241f9eeb7cf19298ad34ce7de4062bd47
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2026-01-19 23:17:10 +0530

    new learning

---
+++ 06_chai_business/main.py
@@ -0,0 +1,8 @@
+# import recepies.flavours
+
+# print(recepies.flavours.elaichi_chai())
+
+from recepies.flavours import elaichi_chai, ginger_chai
+
+print(elaichi_chai())
+print(ginger_chai())
---
+++ 06_chai_business/recepies/flavours.py
@@ -0,0 +1,5 @@
+def elaichi_chai():
+    return "Elaichi Chai: A fragrant blend of black tea, milk, sugar, and crushed green cardamom pods."
+
+def ginger_chai():
+    return "Ginger Chai: A spicy infusion of black tea, milk, sugar, and fresh ginger slices."
