4d0657009f7db6cb442253a8f66d29baf460d56a
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2026-01-20 23:04:24 +0530

    learning

---
+++ 07_comprehensions/01_compre.py
@@ -0,0 +1,12 @@
+menu = [
+    "Masala Chai",
+    "Iced Lemon Tea",
+    "Cold Coffee",
+    "Hot Chocolate",
+    "Green Tea",
+    "Iced Green Tea"
+]
+
+iced_tea = [tea for tea in menu if "Iced" in tea]
+
+print(iced_tea)
