d5357ecea99db24e7a33e9d38b0a574fc5ed88f4
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2026-04-30 06:37:11 +0530

    new learning

--- New_01_Basic/hello_chai.py
+++ New_01_Basic/hello_chai.py
@@ -3,4 +3,9 @@ print("Chai or Python? Let's say 'Hello, Chai!'")
 def chai(n):
     print(n)
 
-chai("Lemon Tea")    
+chai("Lemon Tea")
+
+chai_one = "masala chai"
+chai_two = "green tea"
+chai_three = "black tea"
+chai_four = "ginger tea"
