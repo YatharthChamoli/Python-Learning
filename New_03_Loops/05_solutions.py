a7753ced42a784fd2e9301e77cdb76670a04d721
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2026-06-19 00:15:01 +0530

    practice

--- New_03_Loops/05_solutions.py
+++ New_03_Loops/05_solutions.py
@@ -1,3 +1,10 @@
 # Find the First Non-Repeated Character
 # Problem: Given a string, find the first non-repeated character.
 
+input_Str = "teeter"
+
+for char in input_Str:
+    print(char)
+    if input_Str.count(char) == 1:
+        print("Char is: ", char)
+        # break
