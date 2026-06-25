7774d2aece5eff6fffd30695e0869cc641035bfe
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2026-06-25 23:28:55 +0530

    new learning

---
+++ New_05_Functions/02_solution.py
@@ -0,0 +1,7 @@
+# Function with Multiple Parameters
+# Problem: Create a function that takes two numbers as parameters and returns their sum.
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+print(add_numbers(5,5))    
---
+++ New_05_Functions/04_solution.py
@@ -0,0 +1,14 @@
+# Function Returning Multiple Values
+# Problem: Create a function that returns both the area and circumference of a circle given its radius.
+
+import math
+
+def circle_stats(radius):
+    area = math.pi * radius ** 2
+    circumference = math.pi * 2 * radius
+    return area, circumference
+
+
+a , c = circle_stats(5)
+
+print("Area: ", round(a, 2), "Circumference: ", round(c, 2))
