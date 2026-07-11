a4f00b041ba5d13cef22abe400ea0baa9602443c
Yatharth Chamoli <chamoliyatharth02@gmail.com>
2026-07-11 22:44:07 +0530

    learning

---
+++ New_05_Functions/08_solution.py
@@ -0,0 +1,9 @@
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_kwargs(name="Shaktiman", power="lazer")
+print_kwargs(name="Shaktiman")
+print_kwargs(name="Shaktiman", power="lazer", enemy="Kalicharan")
+
+
